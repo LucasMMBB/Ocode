117d400b1e65d353f0485eb1e373b253db799acd
Yang Tang <frandblinkc@gmail.com>
2017-03-08 02:52:21 -0600

    add client-side access control

--- week3/oj-client/src/app/app.routes.ts
+++ week3/oj-client/src/app/app.routes.ts
@@ -1,29 +1,30 @@
-import { Routes, RouterModule} from "@angular/router";
+import { Routes, RouterModule } from "@angular/router";
 import { ProblemListComponent } from './components/problem-list/problem-list.component';
 import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { AuthGuardService } from './services/auth-guard.service';
 
 const routes: Routes = [
   {
-    path: "",
-    redirectTo: "problems",
-    pathMatch: "full"
+    path: '',
+    redirectTo: 'problems',
+    pathMatch: 'full'
   },
   {
-    path: "problems",
+    path: 'problems',
     component: ProblemListComponent
   },
   {
-    path: "problems/:id",
+    path: 'problems/:id',
     component: ProblemDetailComponent
   },
   {
-    path: "profile",
+    path: 'profile',
+    canActivate: [AuthGuardService],
     component: ProfileComponent
-  },
-  {
-    path: "**",
-    redirectTo: "problems",
+  }, {
+    path: '**',
+    redirectTo: 'problems'
   }
 ];
 
