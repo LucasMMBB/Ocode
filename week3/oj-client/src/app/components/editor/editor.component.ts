e277d5e73e93ae1f13f1abc8ce4ce98714d2cc2f
Yang Tang <frandblinkc@gmail.com>
2017-03-10 00:04:03 -0600

    Add language select, reset and submit buttons

--- week3/oj-client/src/app/components/editor/editor.component.ts
+++ week3/oj-client/src/app/components/editor/editor.component.ts
@@ -11,12 +11,26 @@ export class EditorComponent implements OnInit {
 
   editor: any;
 
+  // languages available and default language
+  public languages: string[] = ['Java', 'C++', 'Python'];
+  language: string = 'Java';
+
   defaultContent = {
-    'java': `public class Example {
-      public static void main(String[] args) {
-          // Type your Java code here
-      }
-    }`
+    'Java': `public class Example {
+  public static void main(String[] args) {
+      // Type your Java code here
+  }
+}`,
+    'C++': `#include <iostream>
+using namespace std;
+
+int main() {
+   // Type your C++ code here
+   return 0;
+}`,
+    'Python': `class Solution:
+    def example():
+        # Write your Python code here`
   };
 
   constructor() { }
@@ -24,8 +38,25 @@ export class EditorComponent implements OnInit {
   ngOnInit() {
     this.editor = ace.edit("editor");
     this.editor.setTheme("ace/theme/eclipse");
-    this.editor.getSession().setMode("ace/mode/java");
-    this.editor.setValue(this.defaultContent['java']);
+    this.resetEditor();
     this.editor.$blockScrolling = Infinity;
+  }
+
+  // reset editor content
+  resetEditor(): void {
+    this.editor.setValue(this.defaultContent[this.language]);
+    this.editor.getSession().setMode("ace/mode/" + this.language.toLowerCase());
+  }
+
+  // set language
+  setLanguage(language: string): void {
+    this.language = language;
+    this.resetEditor();
+  }
+
+  // submit
+  submit(): void {
+    let user_code = this.editor.getValue();
+    console.log(user_code);
   }
 }
