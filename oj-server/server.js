c268a2c31cabebf2c78f004b034a75285a05df45
LucasMMBB <maoxummbb@gmail.com>
2017-07-31 19:31:16 -0400

    init backend server

---
+++ oj-server/server.js
@@ -0,0 +1,9 @@
+var express = require('express')
+var app = express()
+var restRouter = require("./routes/rest")
+
+app.use("/api/v1", restRouter);
+
+app.listen(3000, function(){
+	console.log("App listening on port 3000");
+});
