2d1f77404cb715ac63e71e7f514a8fbab8bb686b
LucasMMBB <maoxummbb@gmail.com>
2017-08-06 21:30:42 -0400

    init DATABASE using mongoose

--- oj-server/server.js
+++ oj-server/server.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var app = express()
 var restRouter = require("./routes/rest")
+var mongoose = require("mongoose");
+
+mongoose.connect('mongodb://user:user@ds019058.mlab.com:19058/coj');
 
 app.use("/api/v1", restRouter);
 
