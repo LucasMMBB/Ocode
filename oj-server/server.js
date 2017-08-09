5dcc72925deab8c96d096d59810fb4dfd8a90f7a
LucasMMBB <maoxummbb@gmail.com>
2017-08-09 11:16:23 -0400

    update server side routing

--- oj-server/server.js
+++ oj-server/server.js
@@ -7,9 +7,14 @@ var path = require('path');
 mongoose.connect('mongodb://user:user@ds019058.mlab.com:19058/coj');
 
 app.use("/", indexRouter);
-app.use("/problems", indexRouter);
 app.use(express.static(path.join(__dirname, '../public')));
 app.use("/api/v1", restRouter);
+
+app.use((req, res) => {
+	// send index.html to client side
+	res.sendFile("index.html", {root: path.join(__dirname, '../public/')});
+});
+
 
 app.listen(3000, function(){
 	console.log("App listening on port 3000");
