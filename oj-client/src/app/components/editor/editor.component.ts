536c101125f72063950809971c13ea1b3f1bf832
LucasMMBB <maoxummbb@gmail.com>
2017-08-14 10:06:18 -0400

    surport multi-user collaborative coding and apply changes from others

--- oj-client/src/app/components/editor/editor.component.ts
+++ oj-client/src/app/components/editor/editor.component.ts
@@ -1,21 +1,29 @@
-import { Component, OnInit, Inject } from '@angular/core';
+
+import {Component, OnInit, Inject} from '@angular/core';
+
+import {ActivatedRoute, Params} from '@angular/router';
 
 declare var ace: any;
+
 
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.css']
 })
+
+
 export class EditorComponent implements OnInit {
 
   editor: any;
 
-  languages: string[] = ['Java', 'Python', 'C++', 'JavaScript'];
-  language: string = 'Python';
+  sessionId: string;
 
-  themes: string[] = ['Monokai', 'Github', 'Eclipse', 'Xcode', 'textmate'];
-  theme: string = 'Monokai';
+  languages: string[] = ['Java', 'Python', 'C++', 'JavaScript'];
+  language: string = 'JavaScript';
+
+  themes: string[] = ['Monokai', 'Github', 'Eclipse', 'Xcode', 'Terminal', 'Textmate'];
+  theme: string = 'Eclipse';
 
   defaultContent = {
 	  'Python': `class Solution(object):
@@ -48,20 +56,51 @@ public:
 };`
   }
 
-  constructor(@Inject('collaboration') private collaboration) { 
+  constructor(@Inject('collaboration') private collaboration,
+                          private route: ActivatedRoute) { 
   }
 
   ngOnInit() {
-  	this.editor = ace.edit('editor');
-  	this.resetEditor();
-  	this.editor.$blockScrolling = Infinity;
+    // question id send back to server
+    this.route.params
+          .subscribe(params => {
+            this.sessionId = params['id'];
+            this.initEditor();
+          });
+
+    //this.initEditor();
+
+  }
+
+  initEditor(): void {
+    // editor set up
+    this.editor = ace.edit('editor');
+    this.resetEditor();
+    this.editor.$blockScrolling = Infinity;
     this.collaboration.init();
+
+    document.getElementById('editor').focus();
+
+
+    this.collaboration.init(this.editor, this.sessionId);
+    this.editor.lastAppliedChange = null;
+
+    this.editor.on('change', (e)=>{
+      console.log('editor changes: ' + JSON.stringify(e));
+      
+      if(this.editor.lastAppliedChange != e){
+          this.collaboration.change(JSON.stringify(e));
+      }
+      
+    });
   }
 
   resetEditor(): void {
   	this.editor.getSession().setMode('ace/mode/' + this.language.toLowerCase());
   	this.editor.setTheme('ace/theme/' + this.theme.toLowerCase());
   	this.editor.setValue(this.defaultContent[this.language]);
+    this.editor.gotoLine(this.editor.session.getLength());
+    //this.editor.setHighlightActiveLine(true);
   }
 
   setTheme(theme: string): void {
--- oj-server/services/SocketService.js
+++ oj-server/services/SocketService.js
@@ -1,11 +1,42 @@
 module.exports = function(io) {
-	io.on('connection', (socket) => {
-		//console.log(socket);
 
-		var message = socket.handshake.query['message'];
-		console.log("From client: " + message);
+	// collaboration sessions
+	var collaborations = [];
+
+	// map from socketId to sessionId
+	var socketIdToSessionId = []
+
+	io.on('connection', (socket) =>{
+		let sessionId = socket.handshake.query['sessionId'];
+
+		socketIdToSessionId[socket.id] = sessionId;
+
+		// add socket.id to corresponding collaboration session participants
+		if(!(sessionId in collaborations)){
+			collaborations[sessionId] = {
+				'participants' : []
+			}
+		}else{
+			collaborations[sessionId]['participants'].push(socket.id);
+		}
+
+		// socket event listeners
+		socket.on('change', (delta) => {
+			console.log("change " + socketIdToSessionId[socket.id] + " " + delta);
+			let sessionId = socketIdToSessionId[socket.id];
+
+			if(sessionId in collaborations){
+				let participants = collaborations[sessionId]['participants'];
+				for(let i = 0; i < participants.length; i++){
+					if(socket.id != participants[i]){
+						io.to(participants[i]).emit("change", delta);
+					}
+				}
+			} else {
+				console.log("WARNING: could not tie socket_id to any collaborations");
+			}
+		});
 		
-		io.to(socket.id).emit("message", 'hehe from server');
-		socket.emit("message","haha from server!");
-	})
+	});
+
 }
